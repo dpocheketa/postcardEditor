09dce7b0e8ee2409754b11a42294fd2ad813e224
dpocheketa <dpocheketa@gmail.com>
2015-01-27 21:00:06 +0200

    added back side

--- src/app/pages/editor/editor.controller.js
+++ src/app/pages/editor/editor.controller.js
@@ -1,21 +1,43 @@
 'use strict';
 
-angular.module('postcardEditor').controller('EditorCtrl', ['$scope',
-	function($scope){
+angular.module('postcardEditor').controller('EditorCtrl', ['$scope', '$modal',
+	function($scope, $modal){
 		console.log('postcardEditorCtrl');
+
+		var myModal = $modal({
+				title: 'Text block adding',
+				scope: $scope,
+				backdrop: true,
+				contentTemplate: 'components/modal/modal.html',
+				show: false,
+			});
 
 		$scope.settings = {
 			rotate: 0,
 			flip: 0,
-			scale: 1
+			scale: 1,
+			text: {
+				left: [],
+				right: ["Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error molestias soluta pariatur ab perspiciatis, est, quidem temporibus dolores nulla laudantium omnis facere explicabo, eius sit deserunt, aperiam exercitationem. Sint, enim."],
+				center: []
+			}
+		};
+
+		$scope.modal = {
+			textParent: null,
+			newText: ""
 		};
 
 		$scope.zoomIn = function(){
-			$scope.settings.scale += 0.1;
+			if ($scope.settings.scale <= 1.5) {
+				$scope.settings.scale += 0.1;
+			};
 		};
 
 		$scope.zoomOut = function(){
-			$scope.settings.scale -= 0.1;
+			if ($scope.settings.scale >= 0.5) {
+				$scope.settings.scale -= 0.1;
+			};
 		};
 
 		$scope.rotate = function(){
@@ -26,5 +48,17 @@ angular.module('postcardEditor').controller('EditorCtrl', ['$scope',
 			$scope.settings.flip += 180;
 		};
 
+		$scope.addText = function(){
+			myModal.$promise.then(myModal.show);
+		};
+
+		$scope.addTextTo = function(part){
+			var newText = $scope.modal.newText;
+
+			$scope.settings.text[part].push(newText);
+
+			$scope.modal.newText = '';
+			$scope.modal.textParent = null;
+		};
 	}
 ]);
